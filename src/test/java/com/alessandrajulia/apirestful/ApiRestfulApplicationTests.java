986b3a84cc04ffce504b8b7271a95e6f8cb9759d
Alessandra Julia <julialessandr@gmai.com>
2021-05-13 16:20:32 -0300

    First commit

---
+++ src/test/java/com/alessandrajulia/apirestful/ApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alessandrajulia.apirestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
